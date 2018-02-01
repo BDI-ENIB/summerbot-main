7d9f73a973e2a495769b43f74c4bf0258f33662f
titaniumbaryum <titouan.baillon@gmail.com>
2018-02-01 20:47:55 +0100

    removed StepperDriver

--- summerbot-main.ino
+++ summerbot-main.ino
@@ -1,14 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-#include "StepperDriver/src/A4988.h"
-#include "StepperDriver/src/BasicStepperDriver.h"
-#include "StepperDriver/src/DRV8825.h"
-#include "StepperDriver/src/DRV8834.h"
-#include "StepperDriver/src/DRV8880.h"
-#include "StepperDriver/src/MultiDriver.h"
-#include "StepperDriver/src/SyncDriver.h"
-
 #include "summerbot-claw/claw.hpp"
 #include "summerbot-motionbase/Motion_base.hpp"
 #include "pinout.h"
