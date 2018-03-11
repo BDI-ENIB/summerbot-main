786baaf415aa8bd7bda60f29b83c84890fd101cc
titaniumbaryum <titouan.baillon@gmail.com>
2018-03-11 14:50:22 +0100

    updated commands

--- summerbot-main.ino
+++ summerbot-main.ino
@@ -8,11 +8,10 @@
 IntervalTimer motionTimer;
 DualDRV8825* dd=new DualDRV8825(200, 32, 30, 31, 29, 26, 25, 24);// steps per rev,left dir pin, left step pin, right dir pin, right step pin, mode pin 0, mode pin 1, mode pin 2
 MotionBase mb(dd,33,63); // motors, wheel radius, robot radius, x, y, a
-MotionBase mb(left,right,33,63); // left motor, right motor, wheel radius, robot radius, x, y, a
 
 void setup (){
   //Timer
-  motionTimer.begin(motionLoop,100);// 1kHz (100 is the period in microS)
+  motionTimer.begin(motionLoop,100);// 10kHz (100 is the period in microS)
   motionTimer.priority(1); //slightly above normal
   
   //Serial
