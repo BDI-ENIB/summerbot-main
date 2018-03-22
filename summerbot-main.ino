86e1211ab78090b296ab396bdadac7f8bdf7da5f
Julien <34742806+Steins7@users.noreply.github.com>
2018-03-22 18:18:51 +0100

    fixed some more bugs

--- summerbot-main.ino
+++ summerbot-main.ino
@@ -17,16 +17,15 @@ SensorManager* sensorManager;
 double startTime ;
 
 void setup (){
+
+  pinMode(STARTER, INPUT_PULLUP);  
+	
   //SensorManager
   sensorManager = new SensorManager();  
   sensorManager->registerNewSensor(IRS1, SHARP);
   sensorManager->registerNewSensor(IRS2, SHARP);
   sensorManager->registerNewSensor(IRS3, SHARP);
   sensorManager->registerNewSensor(IRS4, SHARP);
-  sensorManager->registerNewSensor(IRS5, SHARP);
-  sensorManager->registerNewSensor(IRS6, SHARP);
-  sensorManager->registerNewSensor(IRS7, SHARP);
-  sensorManager->registerNewSensor(IRS8, SHARP);
   
   //Timer
   motionTimer.begin(motionLoop,100);// 10kHz (100 is the period in microS)
@@ -71,15 +70,11 @@ void loop (){
 			sensorManager->detectObject(IRS1, DISTANCETHRESHOLD) ||
 			sensorManager->detectObject(IRS2, DISTANCETHRESHOLD) ||
 			sensorManager->detectObject(IRS3, DISTANCETHRESHOLD) ||
-			sensorManager->detectObject(IRS4, DISTANCETHRESHOLD) ||
-			sensorManager->detectObject(IRS5, DISTANCETHRESHOLD) ||
-			sensorManager->detectObject(IRS6, DISTANCETHRESHOLD) ||
-			sensorManager->detectObject(IRS7, DISTANCETHRESHOLD) ||
-			sensorManager->detectObject(IRS8, DISTANCETHRESHOLD)){
+			sensorManager->detectObject(IRS4, DISTANCETHRESHOLD)){
 		mb.pause();
 		return;
 	}
-	Serial.println("mb.resume()");
+	Serial.println(mb.movesString());
 	mb.resume();
 }
 void motionLoop(){
