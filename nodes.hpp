cf05f9faa2ce5f5cb990b29265b6707a7408926d
ShinySilver <nicolas.louismarie@gmail.com>
2018-04-26 15:43:31 +0200

    updated robot.h & added some nodes necessary for the cube loading protocol

--- nodes.hpp
+++ nodes.hpp
@@ -15,11 +15,14 @@ class Node {
     }
     Node(int x, int y): x{x}, y{y} {}
 };
+
 Node G_RoadMain(350,610);
 Node G_Start(180,200);
 Node G_Cross1(550,650);
 Node G_Cross2(990,300);
 Node G_Cross3(1500,900);
 
-Node unloadingZones[]={Node(180,500),Node(180,720),Node(180,830)};
+Node unloadingZones[]={Node(180,500), Node(180,720), Node(180,830)};
+Node RoadCubeLoadingZone[]={Node(540,610), Node(1190,610), Node(1500,610)};
+Node RealCubeLoadingZone[]={Node(540,850-ROBOT_1_W_DEPLOYED), Node(1190,280+44+ROBOT_1_W_DEPLOYED), Node(1500,1100-ROBOT_1_W_DEPLOYED)};
 #endif
