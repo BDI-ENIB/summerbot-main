5c77a2ed15a4ddb64086b285b0d6708f1f359535
ShinySilver <nicolas.louismarie@gmail.com>
2018-05-09 14:31:48 +0200

    Roche-sur-Yion ~ 2nd commit

--- src/protocols/panel_activation_protocol.cpp
+++ src/protocols/panel_activation_protocol.cpp
@@ -7,20 +7,27 @@
 void PanelActivationProtocol::update(IA *ia){ //execute the next action of this protocol
   switch(state){
     case 0:
-    ia->mb->moveTo(g_roadButton.getX(), g_roadButton.getY(ia->getFlag("side")),0);
+    ia->mb->moveTo(g_roadMain.getX(), g_roadMain.getY(ia->getFlag("side")),(ia->getFlag("side")?1:-1)*PI/2);
     Serial.println("LOG Starting_PanelActivationProtocol");
     break;
     case 1:
-    ia->mb->translate(-340);
-    Serial.println("LOG going_to_the_button");
+    ia->mb->moveTo(g_roadButton.getX(), g_roadButton.getY(ia->getFlag("side")),0);
     break;
     case 2:
+    ia->mb->translate(ROBOT_1_W/2-g_roadButton.getX()+10);
+    Serial.println("LOG going_to_the_button");
+    break;
+    case 3:
     ia->mb->translateRPM(-20,60);
     Serial.println("LOG smashing_the_button");
     break;
-    case 3:
+    case 4:
     ia->mb->moveTo(g_roadButton.getX(), g_roadButton.getY(ia->getFlag("side")));
     Serial.println("LOG backing_away");
+    break;
+    case 5:
+    ia->mb->moveTo(g_roadMain.getX(), g_roadMain.getY(ia->getFlag("side")),0);
+    Serial.println("LOG Starting_PanelActivationProtocol");
     break;
     default:
     // Anomality
@@ -30,7 +37,7 @@ void PanelActivationProtocol::update(IA *ia){ //execute the next action of this 
 }
 
 bool PanelActivationProtocol::isCompleted(){ //wether the last action of this protocol have already been executed or not
-    return state>3;
+    return state>5;
 }
 
 unsigned short int PanelActivationProtocol::getPriority(IA *ia){
